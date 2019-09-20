8e8104169bab9bd33a752c866c71439a337d8b71
Mohamed Eltahawy <mohamed.abdelrahman.eltahawy@gmail.com>
2019-09-20 11:38:09 +0200

    Add existing file

--- js/main.js
+++ js/main.js
@@ -1,49 +1,64 @@
 $('.slider-test').slick({
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    dots: false,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	dots: false,
 	infinite: false,
 	arrows: true,
-    cssEase: 'linear'
+	cssEase: 'linear',
+	responsive: [
+
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+                            },
+
+
+                          ]
+
 });
 
 
 
 $(function () {
-   
-    "use strict";
-    
-    
-    // Our auto slider code
-    
-    (function autoslider() {
-        
-        $('.slider-testim .active-slider').each(function () {
-           
-            if (!$(this).is(':last-child')) {
-                
-                $(this).delay(3000).fadeOut(1000, function () {
-                  
-                    $(this).
-                        removeClass('active-slider').next().addClass('active-slider').fadeIn();
-                    autoslider();
-                   
-                });
-                
-            } else {
-                
-                $(this).delay(3000).fadeOut(1000, function () {
-                   
-                    $(this).removeClass('active-slider');
-                    $('.slider-testim div').eq(0).addClass('active-slider').fadeIn();
-                    
-                    autoslider();
-                });
-                
-            }
-            
-        });
-        
-    }());
+
+	"use strict";
+
+
+	// Our auto slider code
+
+	(function autoslider() {
+
+		$('.slider-testim .active-slider').each(function () {
+
+			if (!$(this).is(':last-child')) {
+
+				$(this).delay(3000).fadeOut(1000, function () {
+
+					$(this).
+					removeClass('active-slider').next().addClass('active-slider').fadeIn();
+					autoslider();
+
+				});
+
+			} else {
+
+				$(this).delay(3000).fadeOut(1000, function () {
+
+					$(this).removeClass('active-slider');
+					$('.slider-testim div').eq(0).addClass('active-slider').fadeIn();
+
+					autoslider();
+				});
+
+			}
+
+		});
+
+	}());
 
 });
+
+
